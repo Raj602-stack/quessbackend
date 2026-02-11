7a52b5b6e98ce83f1161272139e85d4ae06355ce
Raj602-stack <71328233+Raj602-stack@users.noreply.github.com>
2026-02-11 14:27:20 +0530

    Add debug automatic superuser creation in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,12 +15,18 @@ username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
 email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
 password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
 
+print("DEBUG -> USERNAME:", username)
+print("DEBUG -> EMAIL:", email)
+print("DEBUG -> PASSWORD SET:", bool(password))
+
 if username and password:
     if not User.objects.filter(username=username).exists():
         User.objects.create_superuser(username=username, email=email, password=password)
         print("Superuser created.")
     else:
         print("Superuser already exists.")
+else:
+    print("Superuser environment variables missing!")
 END
 
 echo "Collecting static files..."
