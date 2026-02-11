7de63df57828a826667c0f5ea2f807f67005aa65
Raj602-stack <71328233+Raj602-stack@users.noreply.github.com>
2026-02-11 13:24:00 +0530

    Add missing shebang to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,3 @@
-# #!/bin/sh
-
-# echo "Waiting for database..."
-# sleep 5
-
-# python manage.py migrate
-# python manage.py collectstatic --noinput
-
-# echo "Starting server..."
-# gunicorn core.wsgi:application --bind 0.0.0.0:8000
-
-
 #!/bin/sh
 
 echo "Waiting for database..."
@@ -19,5 +7,4 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 echo "Starting server..."
-gunicorn core.wsgi:application --bind 0.0.0.0:$PORT
-
+exec gunicorn core.wsgi:application --bind 0.0.0.0:$PORT
