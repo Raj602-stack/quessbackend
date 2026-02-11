483000d0ea62d54a8c31bb86eac52c74c1f6fa86
Raj602-stack <71328233+Raj602-stack@users.noreply.github.com>
2026-02-11 12:57:13 +0530

    Fix Railway deployment - use  and update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,22 @@
+# #!/bin/sh
+
+# echo "Waiting for database..."
+# sleep 5
+
+# python manage.py migrate
+# python manage.py collectstatic --noinput
+
+# echo "Starting server..."
+# gunicorn core.wsgi:application --bind 0.0.0.0:8000
+
+
 #!/bin/sh
 
 echo "Waiting for database..."
 sleep 5
 
-python manage.py migrate
+python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 echo "Starting server..."
-gunicorn core.wsgi:application --bind 0.0.0.0:8000
+gunicorn core.wsgi:application --bind 0.0.0.0:$PORT
